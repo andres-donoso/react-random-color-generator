577f370d6aaf020950bee897fa36f422effd8e24
andres-donoso <andres.donoso51@gmail.com>
2022-01-19 07:38:04 +0100

    Created a random color generator with React.

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+// import chalk from 'chalk';
+import randomColor from 'randomcolor';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+  const [hex, setHex] = useState('');
+  // const [luminosity, setLuminosity] = useState('');
+  // const [hue, setHue] = useState('');
+
+  const randomizedHex = () => {
+    const color = randomColor();
+    // luminosity: process.argv[2],
+    // hue: process.argv[3],
+
+    // const color = getRandomColor(), {
+    //   color = "hsl(" + Math.random() * 360 + ", 100%, 75%)"
+    //   return color;
+    setHex(color);
+    // setLuminosity(process.argv[2]);
+    // setHue(process.argv[3]);
+  };
+
+  // });
+  // const luminosity =
+  // const hue =
+
+  // '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: `${hex}`,
+        minHeight: '33.34vh',
+        overflow: 'hidden',
+        zoom: 3,
+      }}
+    >
+      <h1>{hex}</h1>
+      <button onClick={randomizedHex}>Generate</button>
+      <form>
+        <label>
+          {/* <input type="text" value={hue} onChange={() => setLuminosity()} /> */}
+          {''}
+        </label>
+      </form>
+      <form>
+        <label>
+          {/* <input type="text" value={luminosity} onChange={() => setHue()} /> */}
+          {''}
+        </label>
+      </form>
+      <button
+        style={{
+          backgroundColor: `${hex}`,
+        }}
+      >
+        Generated Color: {hex}
+      </button>
+    </div>
+  );
+};
+
+// STYLED COMPONENTS
+// const button = styled.button`
+//   color: white;
+//   padding: 5px 15px;
+//   border-radius: 5px;
+//   outline: 0;
+//   /* text-transform: uppercase; */
+//   margin: 10px 0px;
+//   cursor: pointer;
+//   box-shadow: 0px 2px 2px lightgray;
+//   transition: ease background-color 250ms;
+//   &: :disabled {
+//     cursor: default;
+//     opacity: 0.7;}
+// `;
+export default App;
